745e890ec7f4e336d894920a1b0b53e49bf07ed5
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-05-15 18:08:54 +0200

    Added few entries to the repository to check if it works manually.

--- demo-spring-first/src/main/java/com/examples/spring/demo/first/DemoSpringFirstApplication.java
+++ demo-spring-first/src/main/java/com/examples/spring/demo/first/DemoSpringFirstApplication.java
@@ -1,8 +1,14 @@
 package com.examples.spring.demo.first;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+
+import com.examples.spring.demo.first.model.Person;
+import com.examples.spring.demo.repository.PersonRepository;
 
 //whitout this annotation Autowire can't find the repository
 @EnableJpaRepositories("com.examples.spring.demo.repository")
@@ -11,6 +17,20 @@ public class DemoSpringFirstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringFirstApplication.class, args);
+	}
+	
+	@Component
+	static class RepositoryInitializaer implements CommandLineRunner{
+
+		@Autowired
+		private PersonRepository personRepository;
+		
+		@Override
+		public void run(String... args) throws Exception {
+			personRepository.save(new Person("Mario", "Rossi"));
+			personRepository.save(new Person("Carlo", "Rossi"));
+			personRepository.save(new Person("Paolo", "Bianchi"));
+		}
 	}
 
 }
