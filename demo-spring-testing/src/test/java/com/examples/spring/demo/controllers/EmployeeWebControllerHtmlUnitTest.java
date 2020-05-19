29b5d5c0f646bf46dcbc966cb1ae7f3c175660c4
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-05-19 20:14:48 +0200

    First test for html unit.
    Notice how we need to mock EmployeeService even if it not used by the
    tests.

---
+++ demo-spring-testing/src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
@@ -0,0 +1,40 @@
+package com.examples.spring.demo.controllers;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.examples.spring.demo.services.EmployeeService;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = EmployeeWebController.class)
+public class EmployeeWebControllerHtmlUnitTest {
+
+	@Autowired
+	private WebClient webClient;
+	
+	
+	/* even if not used anywhere, the service must be mocked because we is needed
+	 * by EmployeeWebControllewr  
+	 */
+	@MockBean
+	private EmployeeService employeeService;
+	
+	@Test
+	public void test_homePage_title() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		HtmlPage page = webClient.getPage("/");
+		
+		assertThat(page.getTitleText()).isEqualTo("Employees");
+	}
+}
