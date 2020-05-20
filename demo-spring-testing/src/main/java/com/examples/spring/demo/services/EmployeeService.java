72d65c83f896c91f71f1c982a746cd0e0bc3eb2f
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-05-20 17:21:57 +0200

    Added a fake implementation of EmployeeService for manual testing.

--- demo-spring-testing/src/main/java/com/examples/spring/demo/services/EmployeeService.java
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/services/EmployeeService.java
@@ -1,30 +1,50 @@
 package com.examples.spring.demo.services;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.examples.spring.demo.model.Employee;
 
+/**
+ * Fake implementation to make some manual tests.
+ * Won't be used on automated tests since it will be mocked.
+ *  
+ * @author findus
+ *
+ */
+
 @Service
 public class EmployeeService {
 
-	private static final String TEMPORARY_IMPLEMENTATION = "Temporary Implementation";
+	private Map<Long, Employee> employees = new LinkedHashMap<Long, Employee>();
+	
+	public EmployeeService() {
+		employees.put(1L, new Employee(1L,"Carlo Pedersoli",2000));
+		employees.put(2L, new Employee(1L,"Mario Girotti",1950));		
+	}
 
 	public List<Employee> getAllEmployees() {
-		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+		return new LinkedList<Employee>(employees.values());
 	}
 
 	public Employee getEmployeeById(long id) {
-		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+		return employees.get(id);
 	}
 
 	public Employee insertNewEmployee(Employee employee) {
-		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+		employee.setId(employees.size()+1L);
+		employees.put(employee.getId(), employee);
+		return employee;
 	}
 
 	public Employee updateEmployeeById(long id, Employee employee) {
-		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+		employee.setId(id);
+		employees.put(id, employee);
+		return employee;
 	}
 
 }
