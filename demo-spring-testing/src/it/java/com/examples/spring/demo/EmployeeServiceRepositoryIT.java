56578659a7c2dd029e658c7e760180914f4e7c9f
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-05-21 18:58:08 +0200

    Integration test Service-Repository

---
+++ demo-spring-testing/src/it/java/com/examples/spring/demo/EmployeeServiceRepositoryIT.java
@@ -0,0 +1,52 @@
+package com.examples.spring.demo;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.examples.spring.demo.model.Employee;
+import com.examples.spring.demo.repositories.EmployeeRepository;
+import com.examples.spring.demo.services.EmployeeService;
+
+/**
+ * An integration to verify the interactions between service and repository.
+ * 
+ * @author findus
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Import(EmployeeService.class)
+public class EmployeeServiceRepositoryIT {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Test
+	public void test_service_should_insert_into_repository() {
+		Employee savedEmployee = employeeService.insertNewEmployee(new Employee(null, "Mario", 1000));
+		
+		assertThat(employeeRepository.findById(savedEmployee.getId())).isPresent();
+	}
+
+	@Test
+	public void test_service_can_update_repository() {
+		Employee savedEmployee = employeeRepository.save(new Employee(null, "Carlo", 1000));
+		
+		Employee modEmployee = employeeService.updateEmployeeById(
+				savedEmployee.getId(), new Employee(null, "Mario", 1250));
+		
+		assertThat(employeeRepository.findById(savedEmployee.getId()).get())
+			.isEqualTo(modEmployee);
+	}
+	
+}
