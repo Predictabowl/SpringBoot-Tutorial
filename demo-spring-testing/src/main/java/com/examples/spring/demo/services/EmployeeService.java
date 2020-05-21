85b6f5afdba2918f131d4574f3a264f1b5881cb2
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-05-21 20:17:20 +0200

    Added integration tests for RestController
    Using RestAssured

--- demo-spring-testing/src/main/java/com/examples/spring/demo/services/EmployeeService.java
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/services/EmployeeService.java
@@ -2,9 +2,12 @@ package com.examples.spring.demo.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.examples.spring.demo.model.Employee;
 import com.examples.spring.demo.repositories.EmployeeRepository;
 
+@Service
 public class EmployeeService {
 
 	private EmployeeRepository employeeRepository;
